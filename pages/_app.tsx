42f59a14f891760907ff5e1015e12a99562029dc
mano <ms2geki@gmail.com>
2022-07-16 22:39:33 +0900

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../public/assets/css/tailwind-built.css'
+import '../public/assets/css/animate.min.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
